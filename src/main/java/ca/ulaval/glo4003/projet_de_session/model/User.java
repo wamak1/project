07f613ed7e99bfacd5f1237ad06afd2979fb83a2
wamak1 <wmakssoud@hotmail.com>
2014-09-15 19:40:32 -0400

    class User

--- src/main/java/ca/ulaval/glo4003/projet_de_session/model/User.java
+++ src/main/java/ca/ulaval/glo4003/projet_de_session/model/User.java
@@ -1,1 +1,13 @@
-//Classe User.java
+package ca.ulaval.glo4003.projet_de_session.model;
+
+import java.io.Serializable;
+import java.util.Observable;
+
+public abstract class User extends Observable implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
